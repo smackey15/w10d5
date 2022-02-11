9451eeb9e6a4bb0724659a8953bef1a22ec97075
sean <midruss@yahoo.com>
2022-02-11 17:29:07 -0500

    worked on clock

--- src/clock.jsx
+++ src/clock.jsx
@@ -5,19 +5,22 @@ class Clock extends React.Component {
         super(props); 
         this.state = {time: new Date()}; 
     }
+
+    tick(){
+        // let newDate = {time: new Date()}; 
+        this.setState({time: new Date()}); 
+    }
+
     render(){
         return (
+
             <div>
+                <ul>{setInterval(this.tick(), [delay, 1000])}</ul>
                 <h1>clock</h1>
             </div>
-            
         )
     }
 
-    tick(){
-        let newDate = new Date(); 
-        this.setState({newDate}); 
-    }
 }
 
 export default Clock; 
