2b376a7fa2dd2af7501ec13661111188727d8c4a
sean <midruss@yahoo.com>
2022-02-11 15:57:48 -0500

    finished phase I

---
+++ src/root.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Root() {
+    return(<div>something</div>)
+}
+
+export default Root;
---
+++ widgets.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import Root from "./src/root";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const main = document.getElementById("main");
+    debugger
+    ReactDOM.render(<Root />, main)
+});
